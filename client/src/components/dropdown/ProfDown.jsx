a6a97c8a89e7f7e06ecde7abb4434066cd0dab45
David Surya Ardi <david@192.168.1.6>
2022-05-07 02:33:28 +0700

    login, register, navbar, logout

--- client/src/components/dropdown/ProfDown.jsx
+++ client/src/components/dropdown/ProfDown.jsx
@@ -1,49 +1,86 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import users from "../../images/users.svg";
-import Complaint from "../../images/Complaint.svg";
-import logouts from "../../images/logouts.svg";
-import AddBooks from "../../images/AddBooks.svg";
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import users from '../../images/users.svg';
+import Complaint from '../../images/Complaint.svg';
+import logouts from '../../images/logouts.svg';
+import AddBooks from '../../images/AddBooks.svg';
+import { UserContext } from '../../context/userContext';
 
 export default function ProfDown() {
+  let navigate = useNavigate;
+  const [state, dispatch] = useContext(UserContext);
+  const [isLogin, setIsLogin] = useState(false);
+  const logout = () => {
+    console.log(state);
+    dispatch({
+      type: 'LOGOUT',
+    });
+    navigate('/');
+  };
+
+  console.log(state);
+  let dropDown = '';
+  if (state.user.role == 'admin') {
+    dropDown = (
+      <>
+        <li className="nav-item mb-3">
+          <Link className="dropdown-item fw-bold" to="#">
+            <img
+              src={AddBooks}
+              alt=""
+              style={{ width: '2rem', height: '2rem' }}
+              className="me-3"
+            />
+            Add Book
+          </Link>
+        </li>
+        <li className="nav-item mb-3">
+          <Link className="dropdown-item fw-bold" to="#">
+            <img
+              src={Complaint}
+              alt=""
+              style={{ width: '2rem', height: '2rem' }}
+              className="me-3"
+            />
+            Complain
+          </Link>
+        </li>
+      </>
+    );
+  } else {
+    dropDown = (
+      <>
+        <li className="nav-item mb-3">
+          <Link className="dropdown-item fw-bold" to="/profile">
+            <img
+              src={users}
+              alt=""
+              style={{ width: '2rem', height: '2rem' }}
+              className="me-3"
+            />
+            Profile
+          </Link>
+        </li>
+        <li className="nav-item mb-3">
+          <Link className="dropdown-item fw-bold" to="#">
+            <img
+              src={Complaint}
+              alt=""
+              style={{ width: '2rem', height: '2rem' }}
+              className="me-3"
+            />
+            Complain
+          </Link>
+        </li>
+      </>
+    );
+  }
   return (
     <ul
       className="dropdown-menu navbar-profile"
       aria-labelledby="navbarScrollingDropdown"
     >
-      <li className="nav-item mb-3">
-        <Link className="dropdown-item fw-bold" to="/profile">
-          <img
-            src={users}
-            alt=""
-            style={{ width: "2rem", height: "2rem" }}
-            className="me-3"
-          />
-          Profile
-        </Link>
-      </li>
-      <li className="nav-item mb-3">
-        <Link className="dropdown-item fw-bold" to="#">
-          <img
-            src={AddBooks}
-            alt=""
-            style={{ width: "2rem", height: "2rem" }}
-            className="me-3"
-          />
-          Add Book
-        </Link>
-      </li>
-      <li className="nav-item mb-3">
-        <Link className="dropdown-item fw-bold" to="#">
-          <img
-            src={Complaint}
-            alt=""
-            style={{ width: "2rem", height: "2rem" }}
-            className="me-3"
-          />
-          Complain
-        </Link>
-      </li>
+      {dropDown}
       <li>
         <hr className="dropdown-divider my-2" />
       </li>
@@ -53,8 +90,9 @@ export default function ProfDown() {
           <img
             src={logouts}
             alt=""
-            style={{ width: "2rem", height: "2rem" }}
+            style={{ width: '2rem', height: '2rem' }}
             className="me-3"
+            onClick={logout}
           />
           Logout
         </Link>
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import LogoApp from '../../images/LogoApp.svg';
 import Profile from '../../images/Profile.png';
@@ -7,9 +7,55 @@ import cart from '../../images/cart.png';
 import ProfDown from '../dropdown/ProfDown';
 import Login from '../auth/Login';
 import Register from '../auth/Register';
+import { UserContext } from '../../context/userContext';
 
 export default function Navbar() {
+  const [state, dispatch] = useContext(UserContext);
   const [isLogin, setIsLogin] = useState(false);
+
+  //Navbar
+  let navbar = '';
+  if (state.isLogin) {
+    navbar = (
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center ">
+        <li className="nav-item ">
+          <Link to="/cart" className="nav-link">
+            <img
+              src={cart}
+              alt=""
+              style={{ width: '3rem', height: '3rem' }}
+              className="ms-1 me-lg-4"
+            />
+          </Link>
+        </li>
+        <li className="nav-item dropdown ">
+          <div
+            className="bg-dark rounded-circle overflow-hidden"
+            style={{ width: '4rem', height: '4rem' }}
+            id="navbarScrollingDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img src={Profile} alt="" className="img-fluid" />
+          </div>
+          <ProfDown />
+        </li>
+      </ul>
+    );
+  } else {
+    navbar = (
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center ">
+        <li className="nav-item ">
+          <Login />
+        </li>
+        <li className="nav-item  ">
+          <Register />
+        </li>
+      </ul>
+    );
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light ">
       <div className="container py-lg-4 ">
@@ -28,42 +74,7 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse  " id="navbarTogglerDemo02">
-          {isLogin ? (
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center ">
-              <li className="nav-item ">
-                <Link to="/cart" className="nav-link">
-                  <img
-                    src={cart}
-                    alt=""
-                    style={{ width: '3rem', height: '3rem' }}
-                    className="ms-1 me-lg-4"
-                  />
-                </Link>
-              </li>
-              <li className="nav-item dropdown ">
-                <div
-                  className="bg-dark rounded-circle overflow-hidden"
-                  style={{ width: '4rem', height: '4rem' }}
-                  id="navbarScrollingDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img src={Profile} alt="" className="img-fluid" />
-                </div>
-                <ProfDown />
-              </li>
-            </ul>
-          ) : (
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center ">
-              <li className="nav-item ">
-                <Login />
-              </li>
-              <li className="nav-item  ">
-                <Register />
-              </li>
-            </ul>
-          )}
+          {navbar}
         </div>
       </div>
     </nav>
